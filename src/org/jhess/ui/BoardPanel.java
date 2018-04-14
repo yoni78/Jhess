f4a3268b313076e8e557872386216f5113b3d2e8
yoni78 <yoni788@gmail.com>
2018-04-14 23:08:51 +0300

    Fixed square population.

--- src/org/jhess/ui/BoardPanel.java
+++ src/org/jhess/ui/BoardPanel.java
@@ -1,6 +1,7 @@
 package org.jhess.ui;
 
 import org.jhess.core.Board;
+import org.jhess.utils.SquareUtils;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,22 +10,16 @@ import java.util.List;
 
 class BoardPanel extends JPanel {
 
-    private final List<SquarePanel> boardTiles;
+    private final List<SquarePanel> squarePanels;
     private Board board = new Board();
 
-    private final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+    private final Dimension SQUARE_PANEL_DIMENSION = new Dimension(10, 10);
 
     BoardPanel(Dimension dimension) {
         super(new GridLayout(8,8));
-        boardTiles = new ArrayList<>();
+        squarePanels = new ArrayList<>();
 
-        for (int i = 0; i < 64; i++) {
-            int squareId = 63 - i;
-            SquarePanel tilePanel = new SquarePanel(this, squareId, TILE_PANEL_DIMENSION);
-
-            boardTiles.add(tilePanel);
-            add(tilePanel);
-        }
+        populateSquares();
 
         setPreferredSize(dimension);
         validate();
@@ -32,5 +27,22 @@ class BoardPanel extends JPanel {
 
     Board getBoard(){
         return board;
+    }
+
+    private void populateSquares(){
+        for (int i = 0; i < 64; i++) {
+            int squareRow = SquareUtils.getSquareRow(i);
+            int squareFile = SquareUtils.getSquareFile(i);
+
+            // To reverse the position of the pieces
+            squareRow = 7 - squareRow;
+
+            int squareId = SquareUtils.getSquareId(squareRow, squareFile);
+
+            SquarePanel squarePanel = new SquarePanel(this, squareId, SQUARE_PANEL_DIMENSION);
+
+            squarePanels.add(squarePanel);
+            add(squarePanel);
+        }
     }
 }
--- src/org/jhess/utils/SquareUtils.java
+++ src/org/jhess/utils/SquareUtils.java
@@ -4,20 +4,43 @@ public final class SquareUtils {
     private SquareUtils() {
     }
 
-    public static int getSquareRow(int squareId){
+    /**
+     * Returns the number of the row the square is on by his ID.
+     *
+     * @param squareId The ID of the square.
+     * @return Row number.
+     */
+    public static int getSquareRow(int squareId) {
 
-        if (squareId < 0 || squareId > 63){
+        if (squareId < 0 || squareId > 63) {
             return -1;
         }
 
         return squareId / 8;
     }
 
-    public static int getSquareFile(int squareId){
-        if (squareId < 0 || squareId > 63){
+    /**
+     * Returns the number of the file the square is on by his ID.
+     *
+     * @param squareId The ID of the square.
+     * @return File number.
+     */
+    public static int getSquareFile(int squareId) {
+        if (squareId < 0 || squareId > 63) {
             return -1;
         }
 
         return squareId % 8;
+    }
+
+    /**
+     * Returns the ID of the square by his row and file numbers.
+     *
+     * @param squareRow The number of the row the square is on.
+     * @param squareFile The number of the file the square is on.
+     * @return The ID of the square.
+     */
+    public static int getSquareId(int squareRow, int squareFile) {
+        return squareRow * 8 + squareFile;
     }
 }
